2c75e2e868e41eba412a52ce4a3b4169c4360467
Don Raul <106396891+Villanueva0909@users.noreply.github.com>
2025-05-03 16:20:26 +0800

    Updating router

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <>
+        <div>This is login section</div>
+        </>
+    )
+}
+
+export default Login
---
+++ src/pages/auth/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+    return (
+        <>
+        <div>This is Register section</div>
+        </>
+    )
+}
+
+export default Register
