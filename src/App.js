edeaa9d3ae3e4e617123c1327e4f750fb651db9e
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-04-10 11:06:36 +0800

    Adding Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route, BrowserRouter } from "react-router-dom"
+import Home from "./auth/Home"
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => (
-  <div>
-    <p>react app</p>
-  </div>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
 )
 
 export default App;
