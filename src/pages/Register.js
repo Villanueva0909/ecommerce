d19d0bb8aff69eb36b0d1dcb9ffe0ef2cf385e3b
Raul Villanueva III <106396891+Villanueva0909@users.noreply.github.com>
2026-04-18 15:10:17 +0800

    Updating register form

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,32 @@
-const registerForm = () => <p>register form</p>
+import React, { useState } from "react"
 
-const Register = () => (
-    <div className="container p-5">
-        <div className="row">
-            <div className="col-md-6 offset-md-3">
-                <h4>Register</h4>
-                {registerForm()}
+const Register = () => {
+    const [email, setEmail] = useState()
+    const handleSubmit = () => {
+        //
+    }
+    const registerForm = () => (
+        <form onSubmit={handleSubmit}>
+            <input type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                autoFocus />
+
+                <button type="submit" className="btn btn-raised">Submit</button>
+        </form>
+    )
+
+    return (
+        <div className="container p-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <h4>Register</h4>
+                    {registerForm()}
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 export default Register
